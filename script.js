b1e29ee596eafd3de9bf68e9db29e01f4b0c46b3
Christina Persson <christina@MacBook-Pro.local>
2020-04-09 15:37:02 +0200

    Added clickable images for all social media and grid to organise the employee cards

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-const employeContainer = document.getElementById("employe-container");
+const employeeContainer = document.getElementById("employee-container");
 
 fetch("https://api.tretton37.com/ninjas")
   .then((res) => {
     return res.json();
   })
   .then((json) => {
-    const employeList = json;
+    const employeeList = json;
 
-    const showEmployes = () => {
-      employeContainer.innerHTML = "";
-      employeList.map((employe) => {
-        employeContainer.innerHTML += `<div class="employe-card">
-        <h2>${employe.name}</h2>
-        <img src=${employe.imagePortraitUrl}>
+    const showEmployees = () => {
+      employeeContainer.innerHTML = "";
+      employeeList.map((employee) => {
+        employeeContainer.innerHTML += `
+        <div class="employee-card">
+          <img src=${employee.imagePortraitUrl}>
+          <h2>${employee.name}</h2>
+          <div class="social-media">
+            ${
+              employee.gitHub
+                ? `<a href="https://github.com/${employee.gitHub}"><img class="icon" src="icons/github.svg"></a>`
+                : ""
+            }
+            
+            ${
+              employee.twitter
+                ? `<a href="https://twitter.com/${employee.twitter}"><img class="icon" src="icons/twitter.svg"></a>`
+                : ""
+            }
+            ${
+              employee.stackOverflow
+                ? `<a href="https://stackoverflow.com/users/${employee.stackOverflow}"><img class="icon" src="icons/stackoverflow.svg"></a>`
+                : ""
+            }
+            ${
+              employee.linkedIn
+                ? `<a href="https://www.linkedin.com${employee.linkedIn}"><img class="icon" src="icons/linkedin.svg"></a>`
+                : ""
+            }
+          </div>
         </div>`;
       });
     };
-    showEmployes();
+    showEmployees();
   });
