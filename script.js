53b8f049c3fe298afac132cd9ce083b3e98cf5a3
Christina Persson <christina@MacBook-Pro.local>
2020-04-10 12:33:41 +0200

    Sorting by office and name finished

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const employeeContainer = document.getElementById("employee-container");
+const cityPick = document.querySelector(".office");
 
 fetch("https://api.tretton37.com/ninjas")
   .then((res) => {
     return res.json();
   })
   .then((json) => {
-    const employeeList = json;
+    let employeeList = json;
 
     const showEmployees = () => {
       employeeContainer.innerHTML = "";
@@ -17,28 +18,41 @@ fetch("https://api.tretton37.com/ninjas")
           <div class="social-media">
             ${
               employee.gitHub
-                ? `<a href="https://github.com/${employee.gitHub}"><img class="icon" src="icons/github.svg"></a>`
+                ? `<a href="https://github.com/${employee.gitHub}"><img class="icon" src="icons/github.svg" alt="Github logo" ></a>`
                 : ""
             }
-            
             ${
               employee.twitter
-                ? `<a href="https://twitter.com/${employee.twitter}"><img class="icon" src="icons/twitter.svg"></a>`
+                ? `<a href="https://twitter.com/${employee.twitter}"><img class="icon" src="icons/twitter.svg" alt="Twitter logo"></a>`
                 : ""
             }
             ${
               employee.stackOverflow
-                ? `<a href="https://stackoverflow.com/users/${employee.stackOverflow}"><img class="icon" src="icons/stackoverflow.svg"></a>`
+                ? `<a href="https://stackoverflow.com/users/${employee.stackOverflow}"><img class="icon" src="icons/stackoverflow.svg" alt="Stackoverflow logo"></a>`
                 : ""
             }
             ${
               employee.linkedIn
-                ? `<a href="https://www.linkedin.com${employee.linkedIn}"><img class="icon" src="icons/linkedin.svg"></a>`
+                ? `<a href="https://www.linkedin.com${employee.linkedIn}"><img class="icon" src="icons/linkedin.svg" alt="Linkedin logo"></a>`
                 : ""
             }
           </div>
         </div>`;
       });
     };
+
+    const filterByOffice = () => {
+      employeeList = employeeList.filter(
+        (employee) => employee.office === cityPick.value
+      );
+      employeeList = employeeList.sort((a, b) => a.name.localeCompare(b.name));
+      showEmployees();
+      employeeList = json;
+    };
+
+    if (cityPick) {
+      cityPick.addEventListener("change", filterByOffice);
+    }
+
     showEmployees();
   });
